519b285b48ee746c99ac9989da1c38fdae75c18a
gauthami1989 <gauthamshara@gmail.com>
2025-03-02 03:51:15 +0530

    Add files via upload
    Day1

---
+++ day1/Chrome.java
@@ -0,0 +1,14 @@
+package week1.day1;
+
+public class Chrome {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+float ChromeVersion=91.0f;
+String ChromeDeveloper="google";
+boolean isBetaVersion= false;
+int releaseYear=2008;
+char shortcutKey='C';
+System.out.println("ChromeVersion is:"+ChromVersion+","+"ChromeDeveloper is:"+ChromeDeveloper+","+"isBetaVersion is:"+isBetaVersion+","releaseYear is:"+releaseYear+","+"shortcut key is:"+shortcutKey);
+}
+}
---
+++ day1/LearnDatatypes.java
@@ -0,0 +1,21 @@
+package week1.day1;
+
+public class LearnDatatypes {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int regNo=456556;
+		boolean isAcar=true;
+		float speed=0.05f;
+		char logo='S';
+		String name="Skoda";
+		long phno= 1234567890l;
+		double capacity=124.5654554656;
+		
+		System.out.println("The logo of CAR is:"+logo);
+		System.out.println("regNo is:"+regNo+""+"name of the car is:"+name);
+		System.out.println("speed is:"+speed+","+"is it a car:"+isAcar);
+
+	}
+
+}
